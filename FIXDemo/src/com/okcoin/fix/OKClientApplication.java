49f21573f942c2c779e2d409363bb204ffb135ee
瑞木浩轩 <1939473404@qq.com>
2015-07-14 19:16:22 +0800

    Update OKClientApplication.java
    调整下样式

--- FIXDemo/src/com/okcoin/fix/OKClientApplication.java
+++ FIXDemo/src/com/okcoin/fix/OKClientApplication.java
@@ -5,8 +5,6 @@ import java.util.Date;
 
 import org.apache.log4j.Logger;
 
-
-
 import quickfix.DoNotSend;
 import quickfix.FieldNotFound;
 import quickfix.IncorrectDataFormat;
@@ -20,14 +18,14 @@ import quickfix.UnsupportedMessageType;
 
 /**
  * 客户端请求实现
+ * 
  * @author OKCOIN
  */
-public class OKClientApplication  implements quickfix.Application {
-	
-	
-	
-	private static final Logger log = Logger.getLogger(OKClientApplication.class);
-	
+public class OKClientApplication implements quickfix.Application {
+
+	private static final Logger log = Logger
+			.getLogger(OKClientApplication.class);
+
 	public void onMessage(Message message, SessionID sessionID)
 			throws FieldNotFound, UnsupportedMessageType, IncorrectTagValue {
 		System.out.println("-----------onMessage-----------");
@@ -51,8 +49,8 @@ public class OKClientApplication  implements quickfix.Application {
 			throws FieldNotFound, IncorrectDataFormat, IncorrectTagValue,
 			UnsupportedMessageType {
 		System.out.println("receivedType:" + msg.getHeader().getString(35));
-		System.out.println(sessionID + "------client fromApp-----消息接收----"+new Date().toLocaleString()+"  "
-				+ msg.toString());
+		System.out.println(sessionID + "------client fromApp-----消息接收----"
+				+ new Date().toLocaleString() + "  " + msg.toString());
 	}
 
 	public void onCreate(SessionID sessionID) {
@@ -61,29 +59,29 @@ public class OKClientApplication  implements quickfix.Application {
 	}
 
 	public void onLogon(final SessionID sessionID) {
-	   
-		//请求深度
+
+		// 请求深度
 		Session session = Session.lookupSession(sessionID);
-        Message message =  OKMarketDataRequest.createOrderBookRequest();
-	    
-	    //请求行情
-        //message = OKMarketDataRequest.create24HTickerRequest();
-	  
+		Message message = OKMarketDataRequest.createOrderBookRequest();
+
+		// 请求行情
+		// message = OKMarketDataRequest.create24HTickerRequest();
+
 		// 用户数据请求
 		// message = OKTradingRequest.createUserAccountRequest();
-		 
-        // 创建新订单
-        //try {
+
+		// 创建新订单
+		// try {
 		// message = OKTradingRequest.createOrderBookRequest();
 		// } catch (IOException e) {}
-		
-		 // 取消订单请求
-	     // message = OKTradingRequest.createOrderCancelRequest();
-		 
-         // 订单状态请求
-		 // message = OKTradingRequest.createOrderStatusRequest();
-        session.send(message);
-	
+
+		// 取消订单请求
+		// message = OKTradingRequest.createOrderCancelRequest();
+
+		// 订单状态请求
+		// message = OKTradingRequest.createOrderStatusRequest();
+		session.send(message);
+
 	}
 
 	public void onLogout(SessionID sessionID) {
@@ -93,10 +91,10 @@ public class OKClientApplication  implements quickfix.Application {
 	}
 
 	public void toAdmin(quickfix.Message msg, SessionID sessionID) {
-		//msg.setField(new StringField(553, MessageValidation.PARTNER));
-     	msg.setField(new StringField(553, AccountUtil.apiKey));
+		// msg.setField(new StringField(553, MessageValidation.PARTNER));
+		msg.setField(new StringField(553, AccountUtil.apiKey));
 		msg.setField(new StringField(554, AccountUtil.sercretKey));
-		
+
 	}
 
 	public void toApp(quickfix.Message msg, SessionID sessionID)
@@ -105,6 +103,5 @@ public class OKClientApplication  implements quickfix.Application {
 				+ msg.toString());
 
 	}
-
 
 }
